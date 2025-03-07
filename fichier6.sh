e6ea812411e3e0c2a2bc18d1491c529cc991f5fe
kadirdolek <kadirdolek@outlook.com>
2025-03-07 13:16:28 +0100

    exoscript6

--- fichier6.sh
+++ fichier6.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+echo "Tapez ici le nom du fichier pour vérifier si il existe"
+read fichier
+if [ -f $fichier  ]
+then
+    echo "Le fichier existe."
+else
+    echo "Le fichier n'existe pas."
+fi
